cb16d5bd7ecdc74396b4628d0bf28f9d13073d47
Jeremie-Beaupre <jebea36@ulaval.ca>
2024-02-01 11:23:57 -0500

    Creation des fichiers fonctions

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
 #include <LiquidCrystal.h>
 #include <Arduino.h>
+#include "function.h"
 
 // select the pins used on the LCD panel
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
 // define some values used by the panel and buttons
 int lcd_key     = 0;
-int adc_key_in  = 0;
+//int adc_key_in  = 0;
 int menu = 0;
 bool buttonPressed = false;
 
@@ -18,20 +19,20 @@ bool buttonPressed = false;
 #define btnNONE   5
 
 // read the buttons
-int read_LCD_buttons()
-{
-  adc_key_in = analogRead(0);      // read the value from the sensor
-  Serial.println(adc_key_in);
-  // my buttons when read are centered at these valies: 0, 144, 329, 504, 741
-  // we add approx 50 to those values and check to see if we are close
-  if (adc_key_in > 1500) return btnNONE; // We make this the 1st option for speed reasons since it will be the most likely result
-  if (adc_key_in < 50)   return btnRIGHT;  
-  if (adc_key_in < 195)  return btnUP; 
-  if (adc_key_in < 380)  return btnDOWN; 
-  if (adc_key_in < 500)  return btnLEFT; 
-  if (adc_key_in < 750)  return btnSELECT;   
-  return btnNONE;  // when all others fail, return this...
-}
+// int read_LCD_buttons()
+// {
+//   adc_key_in = analogRead(0);      // read the value from the sensor
+//   Serial.println("f");
+//   // my buttons when read are centered at these valies: 0, 144, 329, 504, 741
+//   // we add approx 50 to those values and check to see if we are close
+//   if (adc_key_in > 1500) return btnNONE; // We make this the 1st option for speed reasons since it will be the most likely result
+//   if (adc_key_in < 50)   return btnRIGHT;  
+//   if (adc_key_in < 195)  return btnUP; 
+//   if (adc_key_in < 380)  return btnDOWN; 
+//   if (adc_key_in < 500)  return btnLEFT; 
+//   if (adc_key_in < 750)  return btnSELECT;   
+//   return btnNONE;  // when all others fail, return this...
+// }
 
 
 void setup()
