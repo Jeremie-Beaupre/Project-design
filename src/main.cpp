f24e38f06cff4e5e8f493f782fbc4ceff72a9a78
Jeremie-Beaupre <jebea36@ulaval.ca>
2024-02-14 16:01:09 -0500

    moyenne dans le temps

--- src/main.cpp
+++ src/main.cpp
@@ -23,20 +23,31 @@ float consigne = 300;
 float position_lame;
 float erreur;
 float volt_solenoide;
-
+int position_lame_moyenne;
+int moy[10];
+int i;
+int mean = 0;
 
 
 void setup()
 {
+  i=0;
   Serial.begin(9600);
   analogWrite(PIN_out_solenoide, 0);
   lcd.begin(16, 2);             
   lcd.setCursor(0,0);
   lcd.print("Superbe Balance ");
-  consigne = analogRead(PIN_in_position); 
+  consigne = analogRead(PIN_in_position);
   //digitalWrite(PIN_3V, HIGH);
 }
 
+float calculateMean(int arr[], int size) {
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += arr[i];
+    }
+    return (float)sum / size;
+}
 
 void loop()
 {
@@ -44,6 +55,14 @@ void loop()
   position_lame = analogRead(PIN_in_position); //position de la lame
   erreur = position_lame - consigne; //erreur de la position de la lame 
   volt_solenoide = pid(erreur, kp, ki, kd); //tension a fournir au solenoide
+
+  //Changer le menu affiché
+  moy[i] = volt_solenoide;
+  if(i%10){
+    mean = calculateMean(moy, 10);
+    i=0;
+  }
+  i++;
   if (volt_solenoide >= 255){
     volt_solenoide =255;
   }
@@ -58,7 +77,7 @@ void loop()
   Serial.print("solenoide: ");
   Serial.println(volt_solenoide);
   //delay(2000);
-  //Changer le menu affiché
+
   lcd_key = read_LCD_buttons();
   choose_menu(menu, lcd_key, buttonPressed);
   lcd.setCursor(0,1);
